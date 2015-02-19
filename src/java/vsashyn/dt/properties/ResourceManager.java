5d92c39fe1bd79acb5e387ad9cab345f2c3e86eb
Vitalii <sashyn.v@gmail.com>
2015-02-19 12:11:30 +0200

    devTeam: add properties bundles

---
+++ src/java/vsashyn/dt/properties/ResourceManager.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vsashyn.dt.properties;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ *
+ * @author vsa
+ */
+public enum ResourceManager {
+     INSTANCE;
+     private ResourceBundle resourceBundle;
+     private final String resourceName = "content";
+     private ResourceManager(){
+         resourceBundle = ResourceBundle.getBundle(resourceName, Locale.getDefault());
+     }
+    public void changeResource(Locale locale){
+        resourceBundle = ResourceBundle.getBundle(resourceName, locale);
+    } 
+    public String getString(String key){
+        return resourceBundle.getString(key);
+    }
+}
